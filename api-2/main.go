abc9f93e78bd408f92407d958fa82b809e760ea3
Utkarsh Raj Srivastava <utkarshrajsrivastava7@gmail.com>
2025-04-11 09:39:39 +0530

    input validation still not clear

--- api-2/main.go
+++ api-2/main.go
@@ -6,10 +6,25 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type User struct {
+	Name string `json:"name" binding:"required,min=2"`
+	Age  int    `json:"age" binding:"gte=18,lte=120"`
+}
+
 func main() {
 	router := gin.Default()
+
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"Message": "Hello from Gin"})
+	})
+
+	router.POST("/", func(c *gin.Context) {
+		var user User
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(400, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(200, gin.H{"message": "User is valid", "user": user})
 	})
 
 	router.Run(":8000")
