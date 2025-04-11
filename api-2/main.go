08de9cf84dd9c8931e5f1bb4a7d86c13b3ef48f9
Utkarsh Raj Srivastava <utkarshrajsrivastava7@gmail.com>
2025-04-11 09:12:46 +0530

    gin added

--- api-2/main.go
+++ api-2/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"Message": "Hello from Gin"})
+	})
+
+	router.Run(":8000")
 }
