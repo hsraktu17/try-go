80f078213c1d07a10cef41603f64316775f297b9
Utkarsh Raj Srivastava <utkarshrajsrivastava7@gmail.com>
2025-04-12 18:36:28 +0530

    api-2 done

--- api-2/main.go
+++ api-2/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"sync"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,6 +11,9 @@ type User struct {
 	Name string `json:"name" binding:"required,min=2"`
 	Age  int    `json:"age" binding:"gte=18,lte=120"`
 }
+
+var users []User
+var mu sync.Mutex
 
 func main() {
 	router := gin.Default()
@@ -24,7 +28,14 @@ func main() {
 			c.JSON(400, gin.H{"error": err.Error()})
 			return
 		}
+		mu.Lock()
+		users = append(users, user)
+		mu.Unlock()
 		c.JSON(200, gin.H{"message": "User is valid", "user": user})
+	})
+
+	router.GET("/users", func(c *gin.Context) {
+		c.JSON(http.StatusAccepted, gin.H{"user": users})
 	})
 
 	router.Run(":8000")
